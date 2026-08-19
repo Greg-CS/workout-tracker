f1461839179364847a6545986b0aa6a5742b125f
Greg-466 <gregor.gr20@gmail.com>
2026-08-18 22:41:51 -0400

    TWEAKED: sign in ui fix

--- app/sign-up/[[...slug]]/page.tsx
+++ app/sign-up/[[...slug]]/page.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 export default function SignUpPage() {
   return (
-    <div className="grid md:flex min-h-[75dvh] justify-center items-center">
-      <div className="w-[50%] flex items-center justify-center">
+    <div className="pt-12 grid gap-4 md:gap-0 md:flex min-h-[75dvh] justify-center items-center">
+      <div className="md:w-[50%] flex items-center justify-center">
         <SignUp />
       </div>
-      <div className="w-[50%] flex items-center justify-center">
+      <div className="m-4 md:m-0 md:w-[50%] flex items-center justify-center">
         <Image
           src="/app_images/login-pic.jpg"
           alt="Gym snooze"
